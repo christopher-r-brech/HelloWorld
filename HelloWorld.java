dd994fa1dc2b3e6df24fecf5037f2ee4e46967c1
christopher-r-brech <chris_brech@hotmail.com>
2018-09-24 00:37:33 -0700

    Added Comments

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,14 @@
 public class HelloWorld {
+	
+   // Start main Program Loop
    public static void main(String[] args) {
-      /* Type your code here. */
-	  System.out.println("Hello World");
+	/* ##################################
+	   ###        Hello World         ###
+	   ### Basic Practice Sept24 2018 ###
+	   ################################## */
+		 
+	// Output our customary greeting.
+	System.out.println("Hello World");
       
    }
 }
